8b12ea5548f47c209357c895e42fa4c545a707c9
R Steiner <r.steiner@uconn.edu>
2016-09-28 20:37:51 -0400

    Add text to entry buttons and resize

--- cpp/more/entry.cpp
+++ cpp/more/entry.cpp
@@ -15,8 +15,10 @@ Entry::Entry(QDialog *parent)
 void Entry::populate() {
     QTextEdit *notes = new QTextEdit(this);
     QLineEdit *tags = new QLineEdit(this);
-    QPushButton *closeBtn = new QPushButton(this);
-    QPushButton *submitBtn = new QPushButton(this);
+    QPushButton *closeBtn = new QPushButton("Close", this);
+    QPushButton *submitBtn = new QPushButton("Submit", this);
+    closeBtn->setFixedSize(157, 23);
+    submitBtn->setFixedSize(157, 23);
     this->frm->addRow("Notes:", notes);
     this->frm->addRow("Tags (separated by commas):", tags);
     this->frm->addRow(closeBtn, submitBtn);
